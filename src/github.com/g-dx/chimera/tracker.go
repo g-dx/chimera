b14f1643a45933a56de82c5231649ada9a07595f
g-dx <g-dx@users.noreply.github.com>
2014-03-26 17:29:15 +0000

    Started work on tracker request & response

---
+++ src/github.com/g-dx/chimera/tracker.go
@@ -0,0 +1,34 @@
+package chimera
+
+import (
+	"net"
+	"time"
+	"io/ioutil"
+)
+
+type Response struct {
+
+}
+
+func Request(url string, port int, numWanted int) Response {
+
+	// Connect
+	conn, err := net.Dial("tcp", buildUrl())
+	if err != nil {
+		// TODO: Return empty response?
+	}
+	defer conn.Close()
+
+	// Read response
+	conn.SetReadDeadline(time.Now().Add(time.Second * 10)) // timeout
+	response, err := ioutil.ReadAll(conn)
+	if err != nil {
+		// TODO: Return empty response?
+	}
+
+	// Parse response
+}
+
+func buildUrl() string {
+
+}
