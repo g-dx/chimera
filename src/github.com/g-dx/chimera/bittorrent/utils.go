f17a0e711133a4b42d86734f22989a0caa50dc0c
g-dx <gary.duprex@gmail.com>
2014-03-27 00:43:08 +0000

    Split helper functions out to utils.go

---
+++ src/github.com/g-dx/chimera/bittorrent/utils.go
@@ -0,0 +1,68 @@
+package bittorrent
+
+import (
+	"errors"
+	"fmt"
+)
+
+func optBs(entries map[string] interface {}, key string) (string) {
+	v, ok := entries[key]
+	if !ok {
+		return ""
+	}
+	return v.(string)
+}
+
+func optI(entries map[string] interface {}, key string) (int64) {
+
+	v, ok := entries[key]
+	if !ok {
+		return 0
+	}
+	return v.(int64)
+}
+
+func optL(entries map[string] interface {}, key string) ([]interface {}) {
+
+	v, ok := entries[key]
+	if !ok {
+		return nil
+	}
+	return v.([]interface {})
+}
+
+func bs(entries map[string] interface{}, key string) (string) {
+
+	v, ok := entries[key]
+	if !ok {
+		panic(errors.New(fmt.Sprintf("Mandatory bytestring (%v) not found.", key)))
+	}
+	return v.(string)
+}
+
+func l(entries map[string] interface {}, key string) ([]interface {}) {
+
+	v, ok := entries[key]
+	if !ok {
+		panic(errors.New(fmt.Sprintf("Mandatory list (%v) not found.", key)))
+	}
+	return v.([]interface {})
+}
+
+func i(entries map[string] interface {}, key string) (int64) {
+
+	v, ok := entries[key]
+	if !ok {
+		panic(errors.New(fmt.Sprintf("Mandatory integer (%v) not found.", key)))
+	}
+	return v.(int64)
+}
+
+func d(entries map[string] interface {}, key string) (map[string] interface {}) {
+
+	v, ok := entries[key]
+	if !ok {
+		panic(errors.New(fmt.Sprintf("Mandatory dictionary (%v) not found.", key)))
+	}
+	return v.(map[string] interface {})
+}
