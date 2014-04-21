9ae79a0bac979c62e289db9bf063a50c7dae65ff
g-dx <g-dx@users.noreply.github.com>
2014-04-21 23:00:58 +0100

    Added a bitset implementation for the bitfield.

---
+++ src/github.com/g-dx/chimera/bittorrent/bitset.go
@@ -0,0 +1,75 @@
+package bittorrent
+
+import (
+	"fmt"
+	"bytes"
+)
+
+var bitMasks = [8]uint8{ 1, 2, 4, 8, 16, 32, 64, 128 }
+
+type BitSet struct {
+	bits []uint8
+	size uint32
+	complete bool
+}
+
+func NewBitSet(size uint32) *BitSet {
+
+	// Ensure we have enough storage
+	len := size/8
+	if size%8 != 0 {
+		len++
+	}
+
+	return &BitSet { make([]uint8, len), size, false }
+}
+
+func (bs BitSet) Have(i uint32) bool {
+	if !bs.IsValid(i) {
+		return false
+	}
+
+	return (bs.bits[i/8] & bitMasks[i%8]) == bitMasks[i%8]
+}
+
+func (bs *BitSet) Set(i uint32) {
+	if bs.IsValid(i) && !bs.Have(i) {
+		bs.bits[i/8] = bs.bits[i/8] | bitMasks[i%8]
+	}
+}
+
+func (bs BitSet) IsValid(i uint32) bool {
+	return i >= 0 && i < bs.size
+}
+
+func (bs *BitSet) IsComplete() bool {
+
+	// Fast path
+	if bs.complete {
+		return true
+	}
+
+	// Check all but last byte
+	for i := 0 ; i < len(bs.bits)-2 ; i++ {
+		if bs.bits[i] != 0xFF {
+			return false
+		}
+	}
+
+	// Check last byte
+	b := uint8(8)
+	if bs.size%8 != 0 {
+		b = uint8(bs.size%8)
+	}
+	bs.complete = bs.bits[len(bs.bits)-1] == (0xFF >> (8-b))
+	return bs.complete
+}
+
+func (bs BitSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteString(fmt.Sprintf("BitSet, size=%v ", bs.size))
+	for i := len(bs.bits)-1 ; i >= 0 ; i-- {
+		buf.WriteString(fmt.Sprintf("[%v]%08b", 8*(i+1), bs.bits[i]))
+	}
+	return buf.String()
+}
