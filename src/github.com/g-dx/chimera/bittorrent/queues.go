95b8f957769752da01d5590e2dc494f097f7c55e
g-dx <gary.duprex@gmail.com>
2014-05-01 14:07:48 +0100

    Renamed struct fields

--- src/github.com/g-dx/chimera/bittorrent/queues.go
+++ src/github.com/g-dx/chimera/bittorrent/queues.go
@@ -1,37 +1,37 @@
 package bittorrent
 
 type PeerRequestQueue struct {
-	newRequests     []*RequestMessage
-	pendingRequests []*RequestMessage
-	receivedBlocks  []*BlockMessage
-	maxRequests int
+	new []*RequestMessage
+	pending []*RequestMessage
+	received  []*BlockMessage
+	max int
 }
 
-func NewPeerRequestQueue(maxRequests int) *PeerRequestQueue {
+func NewPeerRequestQueue(max int) *PeerRequestQueue {
 	return &PeerRequestQueue {
-		newRequests: make([]*RequestMessage, 0, maxRequests),
-		pendingRequests: make([]*RequestMessage, 0, maxRequests),
-		maxRequests : maxRequests,
+		new: make([]*RequestMessage, 0, max),
+		pending: make([]*RequestMessage, 0, max),
+		max : max,
 	}
 }
 
 func (pq * PeerRequestQueue) Clear() {
 	// NOTE: This does not make the elements of the slice eligible for GC!
-	pq.newRequests = pq.newRequests[:0]
-	pq.pendingRequests = pq.pendingRequests[:0]
+	pq.new = pq.new[:0]
+	pq.pending = pq.pending[:0]
 }
 
 func (pq * PeerRequestQueue) Remove(index, begin, length uint32) bool {
-	for i, req := range pq.newRequests {
+	for i, req := range pq.new {
 		if req.Index() == index && req.Begin() == begin && req.Length() == length {
-			pq.newRequests = append(pq.newRequests[:i], pq.newRequests[i+1:]...)
+			pq.new = append(pq.new[:i], pq.new[i+1:]...)
 			return true
 		}
 	}
 
-	for i, req := range pq.pendingRequests {
+	for i, req := range pq.pending {
 		if req.Index() == index && req.Begin() == begin && req.Length() == length {
-			pq.pendingRequests = append(pq.pendingRequests[:i], pq.pendingRequests[i+1:]...)
+			pq.pending = append(pq.pending[:i], pq.pending[i+1:]...)
 			return true
 		}
 	}
@@ -40,11 +40,11 @@ func (pq * PeerRequestQueue) Remove(index, begin, length uint32) bool {
 }
 
 func (pq * PeerRequestQueue) IsFull() bool {
-	return len(pq.newRequests) + len(pq.pendingRequests) < pq.maxRequests
+	return len(pq.new) + len(pq.pending) < pq.max
 }
 
 func (pq * PeerRequestQueue) AddRequest(index, begin, length uint32) {
-	pq.newRequests = append(pq.newRequests, Request(index, begin, length))
+	pq.new = append(pq.new, Request(index, begin, length))
 }
 
 func (pq * PeerRequestQueue) AddBlock(index, begin uint32, block []byte) {
