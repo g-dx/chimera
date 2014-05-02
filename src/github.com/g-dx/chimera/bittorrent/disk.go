2d28ce1d49300353d1741e9e51e721e1083c2b8d
g-dx <gary.duprex@gmail.com>
2014-05-02 17:38:59 +0100

    Corrected compile errors.

--- src/github.com/g-dx/chimera/bittorrent/disk.go
+++ src/github.com/g-dx/chimera/bittorrent/disk.go
@@ -2,13 +2,13 @@ package bittorrent
 
 import "log"
 
-func mockDisk(reader chan *RequestMessage, writer chan *BlockMessage, logger *log.Logger) {
+func mockDisk(reader chan<- ProtocolMessage, logger *log.Logger) {
 	for {
 		select {
 		case r := <- reader:
 			logger.Printf("Disk Read: %v\n", r)
-		case w := <- writer:
-			logger.Printf("Disk Write: %v\n", w)
+//		case w := <- writer:
+//			logger.Printf("Disk Write: %v\n", w)
 		}
 	}
 }
